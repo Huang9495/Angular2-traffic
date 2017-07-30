9ddad6b40c17dc190b85d4942f52659d6dfc7ebd
zhouzhou-96 <956544464@qq.com>
2017-07-30 15:40:16 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,34 @@
+import { Http } from '@angular/http';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { AuthService } from './core/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent implements OnInit {
+  car = "";
+  ngOnInit(): void {
+  }
+
+  constructor(private service: AuthService) {
+  }
+
+ onClick(car) {
+ console.log(car);
+ this.AuthService.send(car).subscribe(
+ data => {
+ alert('ok');}, 
+ error => console.log(error));
+ }
+}
+
+
 
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AuthService } from './core/auth.service';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
